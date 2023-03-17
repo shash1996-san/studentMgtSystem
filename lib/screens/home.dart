abb65e2365e2aa417cbdbe3703282db416c1d42c
shash1996-san <shashini.sandamini7@gmail.com>
2023-03-17 12:02:21 +0530

    done functinality of login and sign up fixed bugs

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -5,7 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_application_1/loader.dart';
 import 'package:flutter_application_1/screens/login.dart';
 import "../models/student.dart";
-import 'package:flutter_application_1/Repositories/todoRepo.dart';
+
 import 'package:flutter_application_1/models/student.dart';
 
 class Home extends StatefulWidget {
@@ -61,185 +61,7 @@ class _HomeState extends State<Home> {
           ),
         ],
       ),
-      body: SafeArea(
-        child: StreamBuilder<List<Student>>(
-            stream: RecipiesRepository().listRecipies(),
-            builder: (context, snapshot) {
-              if (!snapshot.hasData) {
-                return const Loader();
-              }
-              List<Recipies>? listRecipies = snapshot.data;
-              return Padding(
-                padding: const EdgeInsets.all(25),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Text(
-                      "All Recipies",
-                      style: TextStyle(
-                        fontSize: 30,
-                        color: Colors.black,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    const SizedBox(height: 10),
-                    Divider(
-                      color: Colors.grey[600],
-                    ),
-                    const SizedBox(height: 20),
-                    ListView.separated(
-                      separatorBuilder: (context, index) => Divider(
-                        color: Colors.grey[600],
-                      ),
-                      shrinkWrap: true,
-                      itemCount: listRecipies!.length,
-                      itemBuilder: (context, index) {
-                        return ListTile(
-                          onTap: () => {
-                            showDialog(
-                              builder: (context) => SimpleDialog(
-                                contentPadding: const EdgeInsets.symmetric(
-                                  horizontal: 25,
-                                  vertical: 20,
-                                ),
-                                backgroundColor: Colors.grey[800],
-                                shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(20),
-                                ),
-                                title: Row(
-                                  children: [
-                                    const Text(
-                                      "Add Task",
-                                      style: TextStyle(
-                                        fontSize: 20,
-                                        color: Colors.white,
-                                      ),
-                                    ),
-                                    const Spacer(),
-                                    IconButton(
-                                      icon: const Icon(
-                                        Icons.cancel,
-                                        color: Colors.grey,
-                                        size: 30,
-                                      ),
-                                      onPressed: () => Navigator.pop(context),
-                                    )
-                                  ],
-                                ),
-                                children: [
-                                  const Divider(),
-                                  TextFormField(
-                                    controller: todoTaskController,
-                                    style: const TextStyle(
-                                      fontSize: 18,
-                                      height: 1.5,
-                                      color: Colors.white,
-                                    ),
-                                    autofocus: true,
-                                    decoration: const InputDecoration(
-                                      hintText: "type your task here",
-                                      hintStyle:
-                                          TextStyle(color: Colors.white70),
-                                      border: InputBorder.none,
-                                    ),
-                                  ),
-                                  TextFormField(
-                                    controller: recipieIngredients,
-                                    style: const TextStyle(
-                                      fontSize: 18,
-                                      height: 1.5,
-                                      color: Colors.white,
-                                    ),
-                                    autofocus: true,
-                                    decoration: const InputDecoration(
-                                      hintText: "type your ingredients here",
-                                      hintStyle:
-                                          TextStyle(color: Colors.white70),
-                                      border: InputBorder.none,
-                                    ),
-                                  ),
-                                  TextFormField(
-                                    controller: recipieDescription,
-                                    style: const TextStyle(
-                                      fontSize: 18,
-                                      height: 1.5,
-                                      color: Colors.white,
-                                    ),
-                                    autofocus: true,
-                                    decoration: const InputDecoration(
-                                      hintText: "type your description here",
-                                      hintStyle:
-                                          TextStyle(color: Colors.white70),
-                                      border: InputBorder.none,
-                                    ),
-                                  ),
-                                  const SizedBox(height: 20),
-                                  SizedBox(
-                                    width: width,
-                                    height: 50,
-                                    child: ElevatedButton(
-                                      onPressed: () async {
-                                        if (todoTaskController
-                                            .text.isNotEmpty) {
-                                          await RecipiesRepository()
-                                              .editRecipie(
-                                                  listRecipies[index].id,
-                                                  todoTaskController.text
-                                                      .trim(),
-                                                  recipieDescription.text
-                                                      .trim(),
-                                                  recipieIngredients.text
-                                                      .trim());
-                                          // ignore: use_build_context_synchronously
-                                          Navigator.pop(context);
-                                        }
-                                      },
-                                      style: ElevatedButton.styleFrom(
-                                        primary: Colors.blue,
-                                        minimumSize: const Size(60, 60),
-                                        elevation: 10,
-                                      ),
-                                      child: const Text("Add"),
-                                    ),
-                                  )
-                                ],
-                              ),
-                              context: context,
-                            ),
-                          },
-                          leading: Text(
-                            listRecipies[index].title,
-                            style: const TextStyle(
-                              fontSize: 25,
-                              color: Colors.black,
-                              fontWeight: FontWeight.w600,
-                            ),
-                          ),
-                          title: Text(
-                            listRecipies[index].ingredients +
-                                ' ' +
-                                listRecipies[index].description,
-                            style: const TextStyle(
-                              fontSize: 25,
-                              color: Colors.black,
-                              fontWeight: FontWeight.w600,
-                            ),
-                          ),
-                          trailing: TextButton(
-                            onPressed: () async {
-                              await RecipiesRepository()
-                                  .removeRecipie(listRecipies[index].id);
-                            },
-                            child: const Icon(Icons.delete),
-                          ),
-                        );
-                      },
-                    )
-                  ],
-                ),
-              );
-            }),
-      ),
+    
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       floatingActionButton: FloatingActionButton(
         backgroundColor: Theme.of(context).primaryColor,
@@ -324,14 +146,7 @@ class _HomeState extends State<Home> {
                   height: 50,
                   child: ElevatedButton(
                     onPressed: () async {
-                      if (todoTaskController.text.isNotEmpty) {
-                        await RecipiesRepository().addRecipie(
-                            todoTaskController.text.trim(),
-                            recipieDescription.text.trim(),
-                            recipieIngredients.text.trim());
-                        // ignore: use_build_context_synchronously
-                        Navigator.pop(context);
-                      }
+                     
                     },
                     style: ElevatedButton.styleFrom(
                       primary: Colors.blue,
