8721982fdbf461ae257e7e6542d22a4b2d2e31a7
shash1996-san <shashini.sandamini7@gmail.com>
2023-03-25 14:03:54 +0530

    added main home modification

--- lib/screens/mainHome.dart
+++ lib/screens/mainHome.dart
@@ -5,9 +5,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter_application_1/loader.dart';
 import 'package:flutter_application_1/screens/login.dart';
 import '../Repositories/contactRepo.dart';
+import 'package:flutter_application_1/screens/home.dart';
+import 'package:flutter_application_1/screens/moduleHome.dart';
+import 'package:flutter_application_1/screens/teacherScreen.dart';
 import "../models/contact.dart";
 
 class MainHomeScreen extends StatefulWidget {
+  const MainHomeScreen({Key? key}) : super(key: key);
+
   @override
   _MainHomeScreenState createState() => _MainHomeScreenState();
 }
@@ -21,7 +26,7 @@ class _MainHomeScreenState extends State<MainHomeScreen>
   late AnimationController _animationController;
   late Animation<Offset> _animation;
 
-   @override
+  @override
   void initState() {
     super.initState();
     _tabController = TabController(length: 3, vsync: this);
@@ -77,115 +82,148 @@ class _MainHomeScreenState extends State<MainHomeScreen>
         ],
         bottom: TabBar(
           controller: _tabController,
-          tabs: [
+          tabs: const [
             Tab(text: 'Manage'),
             Tab(text: 'Contact'),
             Tab(text: 'About'),
           ],
         ),
       ),
-  body: TabBarView(
+      body: TabBarView(
         controller: _tabController,
         children: [
-          AnimatedSwitcher(
-            duration: Duration(seconds:4 ),
-            transitionBuilder: (child, animation) =>
-                ScaleTransition(scale: animation, child: child),
-            child: Container(
-              key: ValueKey<int>(0),
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage("assets/images/img.jpg"),
-                  fit: BoxFit.cover,
+          Padding(
+            padding: const EdgeInsets.all(24.0),
+            child: ListView(
+              children: [
+                const SizedBox(height: 16),
+                Container(
+                  width: 200,
+                  height: 190,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => Home()));
+                    },
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Image.asset(
+                          'lib/assets/student.png',
+                          height: 70,
+                          width: 70,
+                        ),
+                        const SizedBox(height: 16),
+                        const Text(
+                          'Student Home',
+                          style: TextStyle(fontSize: 20),
+                        ),
+                      ],
+                    ),
+                  ),
                 ),
-              ),
-              child: Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    ElevatedButton(
-                      onPressed: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => Home()));
-                      },
-                      child: Text('Student Home'),
+                const SizedBox(height: 16),
+                Container(
+                  width: 200,
+                  height: 190,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => ModuleHome()));
+                    },
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Image.asset(
+                          'lib/assets/module.png',
+                          height: 70,
+                          width: 70,
+                        ),
+                        const SizedBox(height: 16),
+                        const Text(
+                          'Course Modules',
+                          style: TextStyle(fontSize: 20),
+                        ),
+                      ],
                     ),
-                    ElevatedButton(
-                      onPressed: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => Home()));
-                      },
-                      child: Text('Component 2'),
-                    ),
-                    ElevatedButton(
-                      onPressed: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => Home()));
-                      },
-                      child: Text('Component 3'),
-                    ),
-                  ],
+                  ),
                 ),
-              ),
+                const SizedBox(height: 16),
+                Container(
+                  width: 200,
+                  height: 190,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => TeacherScreen()));
+                    },
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Image.asset(
+                          'lib/assets/teacher.png',
+                          height: 70,
+                          width: 70,
+                        ),
+                        const SizedBox(height: 16),
+                        const Text(
+                          'Teacher Home',
+                          style: TextStyle(fontSize: 20),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
-          
-          AnimatedSwitcher(
-            duration: Duration(milliseconds: 500),
-            transitionBuilder: (child, animation) =>
-                FadeTransition(opacity: animation, child: child),
+          Padding(
+            padding: const EdgeInsets.all(24.0),
             child: ListView(
-              key: ValueKey<int>(1),
-            children: [
-              
-              ListTile(
-                leading: Icon(Icons.location_on),
-                title: Text('123 Main St, Gampaha SriLanka'),
-              ),
-              ListTile(
-                leading: Icon(Icons.phone),
-                title: Text('505-565-3445'),
-              ),
-              ListTile(
-                leading: Icon(Icons.email),
-                title: Text('contact@stdmgt.edu'),
-              ),
-              SizedBox(height: 16),
-              Text(
-                'Get in touch with us',
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
+              children: [
+                const ListTile(
+                  leading: Icon(Icons.location_on),
+                  title: Text('123 Main St, Anytown USA'),
                 ),
-              ),
-              SizedBox(height: 16),
-              TextFormField(
-                controller: NameController,
-                decoration: InputDecoration(
-                  labelText: 'Name',
+                const ListTile(
+                  leading: Icon(Icons.phone),
+                  title: Text('555-555-5555'),
                 ),
-              ),
-              TextFormField(
-                controller: EmailController,
-                decoration: InputDecoration(
-                  labelText: 'Email',
+                const ListTile(
+                  leading: Icon(Icons.email),
+                  title: Text('contact@myschool.edu'),
                 ),
-              ),
-              TextFormField(
-                controller: MessageController,
-                decoration: InputDecoration(
-                  labelText: 'Message',
+                const SizedBox(height: 16),
+                const Text(
+                  'Get in touch with us',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-                maxLines: 5,
-              ),
-              SizedBox(height: 16),
-              ElevatedButton(
+                const SizedBox(height: 16),
+                TextFormField(
+                  decoration: const InputDecoration(
+                    labelText: 'Name',
+                  ),
+                ),
+                TextFormField(
+                  decoration: const InputDecoration(
+                    labelText: 'Email',
+                  ),
+                ),
+                TextFormField(
+                  decoration: const InputDecoration(
+                    labelText: 'Message',
+                  ),
+                  maxLines: 5,
+                ),
+                const SizedBox(height: 16),
+                ElevatedButton(
                 onPressed: () async {
                   if (NameController.text.isNotEmpty) {
                     await ContactRepository().addContact(
@@ -216,9 +254,9 @@ class _MainHomeScreenState extends State<MainHomeScreen>
                   }
                 },
                 child: Text('Submit'),
-              ),
-            ],
-          ),
+                ),
+              ],
+            ),
           ),
       Container(
   decoration: BoxDecoration(
@@ -229,27 +267,25 @@ class _MainHomeScreenState extends State<MainHomeScreen>
   ),
   child: Padding(
     padding: EdgeInsets.all(16),
-    child: Column(
-      mainAxisAlignment: MainAxisAlignment.start,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
       children: [
-        Text(
-          'Student Management System',
-          style: TextStyle(
-            fontSize: 24,
-            fontWeight: FontWeight.bold,
+                Text(
+                  'Student Management System',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(height: 16),
+                Text(
+                  'The Student Management System is a software application designed to manage and organize student-related data such as enrollment, academic records, grades, and personal information. It provides a centralized database that facilitates the storage, retrieval, and analysis of this data, helping educational institutions to streamline their administrative processes and improve their overall efficiency.',
+                  textAlign: TextAlign.justify,
+                ),
+              ],
+            ),
           ),
-        ),
-        SizedBox(height: 16),
-        Text(
-          'The Student Management System is a software application designed to manage and organize student-related data such as enrollment, academic records, grades, and personal information. It provides a centralized database that facilitates the storage, retrieval, and analysis of this data, helping educational institutions to streamline their administrative processes and improve their overall efficiency.',
-          textAlign: TextAlign.justify,
-        ),
-      ],
-    ),
-  ),
-)
-
-        ],
+              )        ],
       ),
     );
   }
